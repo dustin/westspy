40374a1a165ad8734c9a7eab06163bea360f82b2
Dustin Sallings <dustin@spy.net>
2016-05-19 23:26:42 -0700

    Fix some lint warnings

--- redirect.go
+++ redirect.go
@@ -25,7 +25,7 @@ var (
 	s3Loc = "s3.amazonaws.com"
 )
 
-func mkUrl(c context.Context, awsId, awsKey, path string, exp int64) string {
+func mkURL(c context.Context, awsID, awsKey, path string, exp int64) string {
 	h := hmac.New(sha1.New, []byte(awsKey))
 	sts := fmt.Sprintf("GET\n\n\n%v\n/%v%v", exp, bucket, path)
 	_, err := h.Write([]byte(sts))
@@ -36,7 +36,7 @@ func mkUrl(c context.Context, awsId, awsKey, path string, exp int64) string {
 	auth := base64.StdEncoding.EncodeToString(h.Sum(nil))
 
 	return fmt.Sprintf("http://%s.%s%s?Signature=%s&Expires=%v&AWSAccessKeyId=%s",
-		bucket, s3Loc, path, url.QueryEscape(auth), exp, awsId)
+		bucket, s3Loc, path, url.QueryEscape(auth), exp, awsID)
 }
 
 func remote(r *http.Request) string {
@@ -50,10 +50,10 @@ func remote(r *http.Request) string {
 func redirect(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 	exp := time.Now().Add(expDuration).Unix()
-	awsId := os.Getenv("AWS_ACCESS_KEY_ID")
+	awsID := os.Getenv("AWS_ACCESS_KEY_ID")
 	awsKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
-	log.Infof(c, "Redirecting %v for %v with key %q", r.URL.Path[7:], remote(r), awsId)
-	http.Redirect(w, r, mkUrl(c, awsId, awsKey, r.URL.Path[7:], exp), 302)
+	log.Infof(c, "Redirecting %v for %v with key %q", r.URL.Path[7:], remote(r), awsID)
+	http.Redirect(w, r, mkURL(c, awsID, awsKey, r.URL.Path[7:], exp), 302)
 }
 
 func init() {
